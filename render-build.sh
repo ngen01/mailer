85250a9a15694fe0d15b484d88c9e7494f002757
Mine Kurtar <mine.kurtar@example.com>
2026-03-17 21:18:59 +0300

    Switch to Docker deployment - Clean up failed native scripts

--- render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-pip install -r requirements.txt
-
-# Install Playwright browsers (without system deps as Render doesn't allow sudo)
-playwright install chromium
